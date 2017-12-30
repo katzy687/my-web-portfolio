e464c550d04b0b1dba581ca7376a0307d2acc910
Natti Katz <katzy687@gmail.com>
2017-12-30 21:18:49 +0200

    github pages test

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,13 @@
 import React from "react";
+import dtMobile from '../assets/DT/DT-Mobile.png';
+
 
 export default () => (
   <div>
     <h1>Check Out the projects homieee</h1>
-   
+    <img src='https://i.imgur.com/oQuQSDN.png' />
+    <img src={dtMobile} alt="mobile image"/>
+    <h1>yoo</h1>
+    
   </div>
 );
