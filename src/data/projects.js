6d2a62f3bccfcdf2131bc2195659fc5730d2decc
Natti Katz <katzy687@gmail.com>
2018-01-09 15:08:02 +0200

    added photos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,11 @@
-import DT from '../assets/client-screens/dank-tickets.png';
+import dtPic from '../assets/client-screens/dt.png';
+import emunahPic from '../assets/client-screens/emunah.png';
+import giliPic from '../assets/client-screens/gili-towing.png';
+import colorsPic from '../assets/client-screens/colors.png';
+import lordOfWarPic from '../assets/client-screens/lord-of-war.png';
+import aStarPic from '../assets/client-screens/a-star.png';
+import fancoPic from '../assets/client-screens/fanco-fan.png';
+
 
 const clientProjects = [
   {
@@ -6,26 +13,28 @@ const clientProjects = [
     description: `Ticketing event site for marijuana conventions. The first project where I incorporated a lot of CSS Grid on the front end. I also hooked up 'Nodemailer' as part of first time user validation flow.`,
     stack: 'AngularJS, mongodb, node, express',
     url: 'https://www.danktickets.herokuapp.com',
-    img: DT
+    img: dtPic
   },
   {
     name: 'Emunah',
     description: 'Community site for Rav Dror of Emunah. Hooks into youtube, twitter and soundcloud apis to bring all content to one place.',
     stack: 'angular, youtube, soundcloud apis',
-    url: 'https://emunah.herokuapp.com/home',
-    img: 
+    url: 'https://emunah.com/home',
+    img: emunahPic
   },
   {
     name: 'Gili\'s Towing',
     description: 'Company landing page for local israeli towing service. First multilingual app. Had to conditonally render rtl or ltr content.',
-    stack: 'React / gatsbyJS',
-    url: 'https://gili-grar.surge.sh'
+    stack: 'React / GatsbyJS',
+    url: 'https://gili-grar.surge.sh',
+    img: giliPic
   },
   {
     name: 'Hebrew Vocabulary Game',
     description: 'Small Matching game done for Language Fountain. I wrote animations for slider on top of ng-animate and synced up audio callbacks from a single audio file using start/stop times.',
     stack: 'angularjs, angular material',
-    url: 'https://katzy687.github.io/hebrew-colors-webpack-ts/#!/'
+    url: 'https://katzy687.github.io/hebrew-colors-webpack-ts/#!/',
+    img: colorsPic
   }
   
 ];
@@ -35,19 +44,22 @@ const personalProjects = [
     name: 'Lord Of War',
     description: 'Small Angular4 project I made for an interview assignment and had a lot of fun with. My first interaction with \'observables\', needed to broadcast changes live across parallel components.',
     stack: 'Angular 4',
-    url: 'https://katzy687.github.io/lord-of-war'
+    url: 'https://katzy687.github.io/lord-of-war',
+    img: lordOfWarPic
   },
   {
     name: 'A-Star-Angular',
-    description: 'Small project incorporating the A* pathfinding algorithm into an angularjs UI (with angular material components). The project was originally an interview assignment task that I continued developing. The task was assigned without proposing an algorithm. I elected to use the A-star or A* over breadth-first-search (BFS) because there is a heuristic of direction that makes it more efficient.',
+    description: 'Small project incorporating the A* pathfinding algorithm into an angularjs UI. The task was an interview assignment, given without proposing an algorithm. I elected to use the A* algorithm over breadth-first-search (BFS) because there is a heuristic of direction that makes it more efficient.',
     stack: 'AngularJS, Angular Material, A-Star Pathfinding module',
-    url: 'https://a-star-angular.herokuapp.com/#!/main'
+    url: 'https://a-star-angular.herokuapp.com/#!/main',
+    img: aStarPic
   },
   {
     name: 'Fanco-Fan',
-    description: 'My final project from coding bootcamp, assigned by IBM. I used D3 to visualize seasonal weather data and correlate it with fan sales for a fictional company. Turns out people don\'t buy fans in the winter. Not that we needed an app for that :). Also hooked into the Twilio API to send messages to our \'agents\' in the field, and the weather API to pull in live Boston Weather.',
+    description: 'My final project from coding bootcamp, assigned by IBM. I used D3 to visualize seasonal weather data and correlate it with fan sales for a fictional company. Turns out people don\'t buy fans in the winter :).',
     stack: 'D3, AngularJS, Twilio, WeatherAPI',
-    url: 'https://fancofan.herokuapp.com/#/home/totalSales'
+    url: 'https://fancofan.herokuapp.com/#/home/totalSales',
+    img: fancoPic
   },
 ];
 
